becfeb265db5916abdc1f65976c39a4c98b16277
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2024-04-19 17:13:30 +0900

    Add input label/text box/button

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+// form div
+let formContainer = document.querySelector("#form");
+
+let sizeSettingText = document.createElement("p");
+formContainer.appendChild(sizeSettingText);
+sizeSettingText.textContent = "New grid size: ";
+let sizeSettingInput = document.createElement("input");
+formContainer.appendChild(sizeSettingInput);
+let submitButton = document.createElement("button");
+formContainer.appendChild(submitButton);
+submitButton.textContent = "Submit";
+
+
+
+// container div
 let gridContainer = document.querySelector("#container");
 
 for (let i=0; i<16; i++) {
-
     let aDiv = document.createElement("div");
     gridContainer.appendChild(aDiv);
     aDiv.addEventListener("mouseover", () => {
         aDiv.style.backgroundColor = "blue";
     })
 }
-
