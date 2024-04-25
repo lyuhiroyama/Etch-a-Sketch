ec622ed1a383afc395b97eb1d03cc04159210724
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2024-04-25 18:34:50 +0900

    Add makeGrid() to dynamically create grid based on user input

--- index.js
+++ index.js
@@ -7,22 +7,48 @@ sizeSettingText.textContent = "New grid size: ";
 let sizeSettingInput = document.createElement("input");
 sizeSettingInput.setAttribute("placeholder", "Enter Grid Size");
 formContainer.appendChild(sizeSettingInput);
+let inputCopy = document.createElement("p");
+formContainer.appendChild(inputCopy);
 let submitButton = document.createElement("button");
 formContainer.appendChild(submitButton);
 submitButton.textContent = "Submit";
-submitButton.addEventListener("click", () => {
-    
-})
 
-
-
+submitButton.addEventListener("click", makeGrid);
+sizeSettingInput.addEventListener("keyup", replicateInput)
 // container div
 let gridContainer = document.querySelector("#container");
 
-for (let i=0; i<16; i++) {
-    let aDiv = document.createElement("div");
-    gridContainer.appendChild(aDiv);
-    aDiv.addEventListener("mouseover", () => {
-        aDiv.style.backgroundColor = "blue";
-    })
+function replicateInput() {
+    let enteredInput = sizeSettingInput.value;
+    inputCopy.textContent = "× " + enteredInput;
 }
+
+function makeGrid() {
+    let enteredInput = sizeSettingInput.value;
+
+    if (enteredInput < 0 || enteredInput > 99 || isNaN(enteredInput)){
+        // Enter code here
+    } else {
+        for (let i=0; i < enteredInput; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            gridContainer.appendChild(row);
+            
+            for (let j=0; j < enteredInput; j++) {
+                let column = document.createElement("div");
+                column.classList.add("column");
+                row.appendChild(column);
+            }
+        }
+    }
+}
+
+
+
+// for (let i=0; i<16; i++) {
+//     let aDiv = document.createElement("div");
+//     gridContainer.appendChild(aDiv);
+//     aDiv.addEventListener("mouseover", () => {
+//         aDiv.style.backgroundColor = "blue";
+//     })
+// }
